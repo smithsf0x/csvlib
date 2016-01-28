56b39f8c4ce9f8a1333ff737f78ce436f0efcbb9
smithsf0x <smithsf0x@gmail.com>
2016-01-28 19:32:51 +0100

    Update csvlib.h

--- csvlib.h
+++ csvlib.h
@@ -4,3 +4,12 @@
 * @Last Modified by:   smithsf0x
 * @Last Modified time: 2016-01-06 14:46:01
 */
+
+#ifndef CSVLIB_H
+#define CSVLIB_H
+
+size_t getline(char **lineptr, size_t *n, FILE *stream);
+int* readCSV(const char * path);
+
+
+#endif
